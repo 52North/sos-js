7af1603835d0c6f770aa05da339fd3c7734e125c
paul-breen <paul.breen6@btinternet.com>
2013-06-26 14:09:28 +0000

    Minor mods. to ws_tester request scripts.  Also added a Proj4js definition for EPSG:32661 (Arctic).

---
+++ 3rd_party_tools/deployed/js/proj4js/lib/defs/EPSG32661.js
@@ -0,0 +1,2 @@
+// Added by Paul M. Breen, BAS, 2013-06-26.  Required for Arctic
+Proj4js.defs["EPSG:32661"] = "+proj=stere +lat_0=90 +lat_ts=90 +lon_0=0 +k=0.994 +x_0=2000000 +y_0=2000000 +ellps=WGS84 +datum=WGS84 +units=m +no_defs";
